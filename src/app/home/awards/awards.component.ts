1509b20d5d7b2f6462970bbcf1beef223fb86768
Chakravarthy <chakravarthy1998@gmail.com>
2023-09-17 19:02:05 +0530

    comi

--- src/app/home/awards/awards.component.ts
+++ src/app/home/awards/awards.component.ts
@@ -17,7 +17,31 @@ export class AwardsComponent implements OnInit, OnChanges {
   
 
 
-  public awardsEnglish: any = [
+  public programing: any = [
+
+    {
+      img: '/assets/images/hack.png',
+      title: 'Angular',
+      name: 'Hackerrank',
+      link: 'https://www.hackerrank.com/certificates/0e494c94e21d'
+    },
+    {
+      img:'/assets/images/hack.png',
+      title: 'Java',
+      name: 'Hackerrank',
+      link:'https://www.hackerrank.com/certificates/d5588274524d'
+
+    },
+    {
+    img:'/assets/images/hack.png',
+    title: 'Mysql',
+    name: 'Hackerrank',
+    link:'https://www.hackerrank.com/certificates/c2428e63985d'
+    }
+
+  ];
+
+  public cloud: any = [
 
     {
       img: '/assets/images/hack.png',
@@ -43,9 +67,13 @@ export class AwardsComponent implements OnInit, OnChanges {
 
 
   
-
+  public skillEnglish: any = [
+    { title: 'Programming', info: this.programing },
+    { title: 'Cloud', info: this.cloud },
+   
+  ];
   
-  public awards: any = this.awardsEnglish;
+  public awards: any = this.skillEnglish;
 
   ngOnInit() {
   }
@@ -54,7 +82,7 @@ export class AwardsComponent implements OnInit, OnChanges {
 
   ngOnChanges() {
     if (this.English) {
-      this.awards = this.awardsEnglish;
+      this.awards = this.skillEnglish;
       this.head = this.headEnglish;
     } 
   }
