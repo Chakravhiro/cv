6ca392062e02f16b757295f1b7ac588379ae4026
Chakravarthy <jc@gmail.com>
2025-10-27 23:23:59 +0530

    add images

--- src/app/home/skills/skills.component.ts
+++ src/app/home/skills/skills.component.ts
@@ -20,19 +20,24 @@ export class SkillsComponent implements OnInit, OnChanges {
   public skill_1: any = [
     [{
       title: 'HTML & CSS',
-      progress: 95
+      progress: 95,
+      image: './assets/images/html-css.jpg'
     }],
     [{
       title: 'Angular',
-      progress: 90
+      progress: 90,
+      image: './assets/images/Angularcli.png'
+
     }],
     [{
       title: 'Typescript',
-      progress: 85
+      progress: 85,
+      image: './assets/images/typescript.png'
     }],
     [{
       title: 'ReactJS',
-      progress: 75
+      progress: 75,
+      image: './assets/images/reactjs.png'
     }]
   ];
 
@@ -40,43 +45,55 @@ export class SkillsComponent implements OnInit, OnChanges {
 
     [{
       title: 'MS Office ',
-      progress: 90
+      progress: 90,
+      image: './assets/images/microsoft.png'
+
+      
     }],
     [{
       title: 'Toad',
-      progress: 95
+      progress: 95,
+      image: './assets/images/Toad.png'
     }],
     [{
       title:'Sql Developer',
-      progress:90
+      progress:90,
+      image: './assets/images/sqldeveloper.jpg'
     }],
     [{
       title:'Confluence,JIRA',
-      progress:90
+      progress:90,
+      image: './assets/images/jira.png'
     }],
     [{
       title:'Postman,Swagger',
-      progress:90
+      progress:90,
+      image: './assets/images/postman.png'
     }],
     [{
       title:'Git',
-      progress:90
+      progress:90,
+      image: './assets/images/git.png'
     }],
     [{
       title:'Maven/Npm',
-      progress:90
+      progress:90,
+      image: './assets/images/maven.png'
     }],
     [{
       title:'Docker',
-      progress:90
+      progress:90,
+      image: './assets/images/docker.png'
     }],
     [{
       title:'Microsoft Azure',
-      progress:90
+      progress:90,
+      image: './assets/images/azure.png'
     }],
     [{
       title:'GCP',
-      progress:90
+      progress:90,
+      image: './assets/images/googlecloud.png'
     }],
 
   ];
@@ -85,31 +102,40 @@ export class SkillsComponent implements OnInit, OnChanges {
 
     [{
       title: 'Core Java',
-      progress: 90
+      progress: 90,
+      image: './assets/images/java.png'
+      
     }],
     [{
       title: 'Spring Boot',
-      progress: 95
+      progress: 95,
+      image: './assets/images/springboot.jpg'
     }],
     [{
       title:'Quarkus',
-      progress:90
+      progress:90,
+      image: './assets/images/quarkus.png'
     }],
     [{
       title:'Spring Data JPA',
-      progress:90
+      progress:90,
+      image: './assets/images/springdata.png'
     }],
     [{
       title:'Spring Security,Keycloak',
-      progress:90
+      progress:90,
+      image: './assets/images/keycloak.png'
+
     }],
     [{
       title:'Linux/Bash Scripting',
-      progress:90
+      progress:90,
+      image: './assets/images/bash.jpg'
     }],
     [{
       title:'Junit Testing',
-      progress:80
+      progress:80,
+      image: './assets/images/junit.png'
     }],
 
   ];
